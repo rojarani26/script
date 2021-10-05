ccd644768959fac22dfd09e1fa001b77128ccc9d
rojarani26 <88879816+rojarani26@users.noreply.github.com>
2021-10-04 17:07:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("Father's Name: Dileep Kumar Naidu ; Mother's Name :Mamata Naidu")
+document.write("Hello , my name is M.Roja Rani and I am from Berhampur,Odisha. I am currently pursuing B.Tech in Civil Engineering under BPUT. Talking about my technical skills, I have learned C,C++ and Java programming language and currently learning Frontend Development at LetsGrowMore.");
